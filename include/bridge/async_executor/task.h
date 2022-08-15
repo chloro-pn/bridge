5b8a1c0bed6eb5e375ceea738f1992001137982e
chloro-pn <13125187405@163.com>
2022-08-15 18:57:38 +0800

    feat: 实现Map结构的协程处理逻辑

--- include/bridge/async_executor/task.h
+++ include/bridge/async_executor/task.h
@@ -6,6 +6,7 @@
 #include "async_simple/coro/Lazy.h"
 #include "bridge/object.h"
 #include "bridge/split_info.h"
+#include "bridge/string_map.h"
 
 namespace bridge {
 
@@ -26,16 +27,20 @@ size_t GetBlockSize(const std::string& content, ObjectType type, size_t begin, c
   return si.GetBlockInfoFromId(id).size();
 }
 
-struct ParseArrayResult {
+struct ParseResult {
   size_t offset;
-  unique_ptr<Array> v;
-  ParseArrayResult(size_t os, unique_ptr<Array>&& obj) : offset(os), v(std::move(obj)) {}
+  unique_ptr<Object> v;
+  ParseResult(size_t os, unique_ptr<Object>&& obj) : offset(os), v(std::move(obj)) {}
 };
 
-Lazy<ParseArrayResult> ParseArray(const std::string& content, const SplitInfo& si, size_t os);
+Lazy<ParseResult> ParseArray(const std::string& content, const SplitInfo& si, const StringMap* sm, size_t os,
+                             bool parse_ref);
+
+Lazy<ParseResult> ParseMap(const std::string& content, const SplitInfo& si, const StringMap* sm, size_t os,
+                           bool parse_ref);
 
 Lazy<std::vector<unique_ptr<Object>>> ParseArrayBlock(const std::string& content, size_t begin, size_t end,
-                                                      const SplitInfo& si) {
+                                                      const SplitInfo& si, bool parse_ref, const StringMap* sm) {
   std::vector<unique_ptr<Object>> ret;
   InnerWrapper w(content);
   w.skip(begin);
@@ -46,16 +51,20 @@ Lazy<std::vector<unique_ptr<Object>>> ParseArrayBlock(const std::string& content
     // 或者只有一个分块信息的Map和Array类型，本地解析
     if (GetBlockSize(content, type, w.currentIndex(), si) <= 1) {
       auto v = ObjectFactory(type);
-      v->valueParse(w, offset, false, nullptr);
+      v->valueParse(w, offset, parse_ref, sm);
       ret.push_back(std::move(v));
     } else {
       if (type == ObjectType::Array) {
-        auto parse_result = co_await ParseArray(content, si, w.currentIndex());
+        auto parse_result = co_await ParseArray(content, si, sm, w.currentIndex(), parse_ref);
         ret.push_back(std::move(parse_result.v));
         w.skipTo(parse_result.offset);
         offset = w.currentIndex();
       } else {
-        assert(false); // parse map not implement from now on
+        assert(type == ObjectType::Map);
+        auto parse_result = co_await ParseMap(content, si, sm, w.currentIndex(), false);
+        ret.push_back(std::move(parse_result.v));
+        w.skipTo(parse_result.offset);
+        offset = w.currentIndex();
       }
     }
   }
@@ -63,19 +72,20 @@ Lazy<std::vector<unique_ptr<Object>>> ParseArrayBlock(const std::string& content
 }
 
 // @brief: 从content[os]处开始解析一个array，不包含ObjectType。
-Lazy<ParseArrayResult> ParseArray(const std::string& content, const SplitInfo& si, size_t os) {
+Lazy<ParseResult> ParseArray(const std::string& content, const SplitInfo& si, const StringMap* sm, size_t os,
+                             bool parse_ref) {
   InnerWrapper w(content);
   w.skip(os);
   size_t offset = os;
   uint64_t count = parseLength(w, offset);
   uint32_t si_id = parseLength(w, offset);
   size_t begin = w.currentIndex();
-  auto block_info = si.GetBlockInfoFromId(si_id);
+  const auto& block_info = si.GetBlockInfoFromId(si_id);
   std::vector<Lazy<std::vector<unique_ptr<Object>>>> tasks;
   for (auto& each : block_info) {
     size_t block_size = each;
     size_t end = begin + block_size;
-    auto task = ParseArrayBlock(content, begin, end, si);
+    auto task = ParseArrayBlock(content, begin, end, si, parse_ref, sm);
     tasks.push_back(std::move(task));
     begin = end;
   }
@@ -88,7 +98,135 @@ Lazy<ParseArrayResult> ParseArray(const std::string& content, const SplitInfo& s
       ret->Insert(std::move(eachobj));
     }
   }
-  co_return ParseArrayResult(begin, std::move(ret));
+  co_return ParseResult(begin, std::move(ret));
+}
+
+Lazy<std::vector<std::pair<std::string, unique_ptr<Object>>>> ParseMapBlock(const std::string& content, size_t begin,
+                                                                            size_t end, const SplitInfo& si,
+                                                                            const StringMap* sm) {
+  std::vector<std::pair<std::string, unique_ptr<Object>>> ret;
+  InnerWrapper w(content);
+  w.skip(begin);
+  size_t offset = begin;
+  while (offset < end) {
+    std::string key;
+    if (sm == nullptr) {
+      uint32_t key_length = parseLength(w, offset);
+      key = std::string(w.curAddr(), key_length);
+      w.skip(key_length);
+      offset += key_length;
+    } else {
+      uint32_t id = parseLength(w, offset);
+      key = sm->GetStr(id);
+    }
+    ObjectType type = parseObjectType(w, offset);
+    if (GetBlockSize(content, type, w.currentIndex(), si) <= 1) {
+      auto v = ObjectFactory(type);
+      v->valueParse(w, offset, false, sm);
+      ret.push_back({std::move(key), std::move(v)});
+    } else {
+      if (type == ObjectType::Array) {
+        auto parse_result = co_await ParseArray(content, si, sm, w.currentIndex(), false);
+        ret.push_back({std::move(key), std::move(parse_result.v)});
+        w.skipTo(parse_result.offset);
+        offset = w.currentIndex();
+      } else {
+        assert(type == ObjectType::Map);
+        auto parse_result = co_await ParseMap(content, si, sm, w.currentIndex(), false);
+        ret.push_back({std::move(key), std::move(parse_result.v)});
+        w.skipTo(parse_result.offset);
+        offset = w.currentIndex();
+      }
+    }
+  }
+  co_return ret;
+}
+
+Lazy<std::vector<std::pair<std::string_view, unique_ptr<Object>>>> ParseMapViewBlock(const std::string& content,
+                                                                                     size_t begin, size_t end,
+                                                                                     const SplitInfo& si,
+                                                                                     const StringMap* sm) {
+  std::vector<std::pair<std::string_view, unique_ptr<Object>>> ret;
+  InnerWrapper w(content);
+  w.skip(begin);
+  size_t offset = begin;
+  while (offset < end) {
+    std::string_view key;
+    if (sm == nullptr) {
+      uint32_t key_length = parseLength(w, offset);
+      key = std::string_view(w.curAddr(), key_length);
+      w.skip(key_length);
+      offset += key_length;
+    } else {
+      uint32_t id = parseLength(w, offset);
+      key = sm->GetStr(id);
+    }
+    ObjectType type = parseObjectType(w, offset);
+    if (GetBlockSize(content, type, w.currentIndex(), si) <= 1) {
+      auto v = ObjectFactory(type);
+      v->valueParse(w, offset, true, sm);
+      ret.push_back({std::move(key), std::move(v)});
+    } else {
+      if (type == ObjectType::Array) {
+        auto parse_result = co_await ParseArray(content, si, sm, w.currentIndex(), true);
+        ret.push_back({std::move(key), std::move(parse_result.v)});
+        w.skipTo(parse_result.offset);
+        offset = w.currentIndex();
+      } else {
+        assert(type == ObjectType::Map);
+        auto parse_result = co_await ParseMap(content, si, sm, w.currentIndex(), true);
+        ret.push_back({std::move(key), std::move(parse_result.v)});
+        w.skipTo(parse_result.offset);
+        offset = w.currentIndex();
+      }
+    }
+  }
+  co_return ret;
+}
+
+Lazy<ParseResult> ParseMap(const std::string& content, const SplitInfo& si, const StringMap* sm, size_t os,
+                           bool parse_ref) {
+  InnerWrapper w(content);
+  w.skip(os);
+  size_t offset = os;
+  uint64_t count = parseLength(w, offset);
+  uint32_t split_id = parseLength(w, offset);
+  const auto& block_info = si.GetBlockInfoFromId(split_id);
+  size_t begin = w.currentIndex();
+
+  if (parse_ref == false) {
+    unique_ptr<Map> ret = map();
+    std::vector<Lazy<std::vector<std::pair<std::string, unique_ptr<Object>>>>> tasks;
+    for (auto& each : block_info) {
+      size_t end = each + begin;
+      auto task = ParseMapBlock(content, begin, end, si, sm);
+      tasks.push_back(std::move(task));
+      begin = end;
+    }
+    auto objs = co_await collectAll(std::move(tasks));
+    for (auto& each_task : objs) {
+      for (auto& each_kv : each_task.value()) {
+        ret->Insert(each_kv.first, std::move(each_kv.second));
+      }
+    }
+    co_return ParseResult(begin, std::move(ret));
+  } else {
+    unique_ptr<MapView> ret = map_view();
+    std::vector<Lazy<std::vector<std::pair<std::string_view, unique_ptr<Object>>>>> tasks;
+    for (auto& each : block_info) {
+      size_t end = each + begin;
+      auto task = ParseMapViewBlock(content, begin, end, si, sm);
+      tasks.push_back(std::move(task));
+      begin = end;
+    }
+    auto objs = co_await collectAll(std::move(tasks));
+    for (auto& each_task : objs) {
+      for (auto& each_kv : each_task.value()) {
+        ret->Insert(each_kv.first, std::move(each_kv.second));
+      }
+    }
+    co_return ParseResult(begin, std::move(ret));
+  }
 }
 
 }  // namespace bridge
--- test/async_simple_test.cc
+++ test/async_simple_test.cc
@@ -15,28 +15,31 @@ TEST(async_simple, basic) {
 
 TEST(async_simple, task) {
   SplitInfo si;
-  Array arr;
+  StringMap sm;
+  Map arr;
   auto a1 = array();
   for (size_t i = 0; i < 10000; ++i) {
     a1->Insert(data("hello" + std::to_string(i)));
   }
-  arr.Insert(std::move(a1));
+  arr.Insert("arr1", std::move(a1));
 
   auto a2 = array();
   for (size_t i = 0; i < 10000; ++i) {
     a2->Insert(data(uint32_t(i)));
   }
-  arr.Insert(std::move(a2));
+  arr.Insert("arr2", std::move(a2));
+
   std::string ret;
-  arr.valueSeri(ret, nullptr, &si);
-  auto new_arr = syncAwait(ParseArray(ret, si, 0));
+  arr.valueSeri(ret, &sm, &si);
+  auto new_arr = syncAwait(ParseMap(ret, si, &sm, 0, true));
   ObjectWrapper w(new_arr.v.get());
-  EXPECT_EQ(w.GetType(), ObjectType::Array);
+  EXPECT_EQ(w.GetType(), ObjectType::Map);
   EXPECT_EQ(w.Size(), 2);
-  EXPECT_EQ(w[0].GetType(), ObjectType::Array);
-  EXPECT_EQ(w[0].Size(), 10000);
-  EXPECT_EQ(w[1].GetType(), ObjectType::Array);
-  for(int i = 0; i < 10000; ++i) {
-    EXPECT_EQ(w[1][i].Get<uint32_t>().value(), i);
+  EXPECT_EQ(w["arr1"].GetType(), ObjectType::Array);
+  EXPECT_EQ(w["arr1"].Size(), 10000);
+  EXPECT_EQ(w["arr2"].GetType(), ObjectType::Array);
+  for (int i = 0; i < 10000; ++i) {
+    EXPECT_EQ(w["arr1"][i].Get<std::string>().value(), "hello" + std::to_string(i));
+    EXPECT_EQ(w["arr2"][i].Get<uint32_t>().value(), i);
   }
 }
